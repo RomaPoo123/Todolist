55160d756dc62ae0a57481f8fa0c5a41e59953be
RomaPoo123 <roma.ivanov96@mail.ru>
2024-05-01 15:37:33 +0300

    Ver. 1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Todolist } from './components/todolist/Todolist';
 
+/*  Типизация большого обьекта, в котором хранятся тудулисты
 
-type TaskType = {
+type TodolistsType = {
+  id:number
+  title: string
+  tasks: Array<TaskType>
+}
+*/
+export type TaskType = {
   id: number
   title: string
   isDone: boolean
 }
 
-export type TodolistsType = {
-  title: string
-  tasks: Array<TaskType>
-}
-
-export let listArray: Array<TodolistsType> = [
-  {
-    title: "What to lean",
-    tasks: [
-      { id: 1, title: "CSS&HTML", isDone: true },
-      { id: 2, title: "JavaScript", isDone: true },
-      { id: 3, title: "React", isDone: false },
-    ]
-  },
-  {
-    title: "What to see",
-    tasks: [
-      { id: 1, title: "Batman", isDone: true },
-      { id: 2, title: "Spider-man", isDone: false },
-      { id: 3, title: "Game of Thrones", isDone: false },
-    ]
-  },
-]
+export type FilterValueType = "All" | "Active" | "Completed"
 
 
 function App() {
+  // Data
+  /* Обьект с тудулистами
+    let listArray: Array<TodolistsType> = [
+      { 
+        id: 1,
+        title: "What to lean",
+        tasks: [
+          { id: 1, title: "CSS&HTML", isDone: true },
+          { id: 2, title: "JavaScript", isDone: true },
+          { id: 3, title: "React", isDone: false },
+        ]
+      },
+      {
+        id: 2,
+        title: "What to see",
+        tasks: [
+          { id: 1, title: "Batman", isDone: true },
+          { id: 2, title: "Spider-man", isDone: false },
+          { id: 3, title: "Game of Thrones", isDone: false },
+        ]
+      },
+    ]
+  */
+  //  logic
+  const [tasks, setTasks] = React.useState([
+    { id: 1, title: "CSS&HTML", isDone: true },
+    { id: 2, title: "JavaScript", isDone: true },
+    { id: 3, title: "React", isDone: false },
+  ])
+  console.log(typeof tasks);
+
+  const [filter, setFilter] = React.useState<FilterValueType>("All")
+
+  const cahgeFilter = (filter: FilterValueType) => {
+    setFilter(filter)
+  }
+
+  function filterTasks(allTask: Array<TaskType>, filter: FilterValueType) {
+    switch (filter) {
+      case "Active":
+        return allTask.filter(task => task.isDone !== true)
+      case "Completed":
+        return allTask.filter(task => task.isDone !== false)
+      default:
+        return allTask
+    }
+  }
+  let FilterNewArr: Array<TaskType> = filterTasks(tasks, filter)
+
+  function removeTask(id: number) {
+    let newTasksState = tasks.filter(task => task.id !== id)
+    setTasks(newTasksState);
+
+  }
+
+
   return (
     <div className="App">
-      {listArray.map(list => {
+      <Todolist title={"what to lean"} tasks={FilterNewArr} removeTask={removeTask} cahgeFilter={cahgeFilter} />
+      {/*  {listArray.map(list => {
         return (
           <Todolist list={list} />
         )
-      })
-      }
-
+      })} */}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/todolist/Todolist.tsx
+++ src/components/todolist/Todolist.tsx
@@ -1,40 +1,42 @@
 import React from "react"
-import { listArray } from "../../App"
-import { TodolistsType } from "../../App"
+import { TaskType, FilterValueType } from "../../App"
 
 
 type TodolistPropsType = {
-    list: TodolistsType
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (id: number) => void
+    cahgeFilter: (filter: FilterValueType) => void
 }
 
 
-export const Todolist = ({ list }: TodolistPropsType) => {
+export const Todolist = ({ title, tasks, removeTask, cahgeFilter }: TodolistPropsType) => {
+
 
     // logic 
-    const tasksList: JSX.Element = list.tasks.length === 0 ? <span>Yor taskslist is empty</span>
+    const tasksList: JSX.Element = tasks.length === 0 ? <span>Yor taskslist is empty</span>
         : <ul>
-            {list.tasks.map(task => <li>
+            {tasks.map(task => <li>
                 <input type="checkbox" checked={task.isDone} />
                 <span>{task.title}</span>
-                <button onClick={() => { alert(task.id) }}>x</button>
+                <button onClick={() => removeTask(task.id)}>x</button>
             </li>
-
             )}
         </ul>
 
     // UI
     return (
         <div>
-            <h3>{list.title}</h3>
+            <h3>{title}</h3>
             <div>
                 <input />
                 <button>+</button>
             </div>
             {tasksList}
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => cahgeFilter("All")}>All</button>
+                <button onClick={() => cahgeFilter("Active")}>Active</button>
+                <button onClick={() => cahgeFilter("Completed")}>Completed</button>
             </div>
         </div>
     )
